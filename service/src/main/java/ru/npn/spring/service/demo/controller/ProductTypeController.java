d72e0d98e8c76cbf5b9daf61f687009c7ab0ef3a
pnovoselov <Novosselov.pavel@gmail.com>
2023-03-14 14:06:28 +0300

    Добавление запроса с фильтрацией querydsl.

--- service/src/main/java/ru/npn/spring/service/demo/controller/ProductTypeController.java
+++ service/src/main/java/ru/npn/spring/service/demo/controller/ProductTypeController.java
@@ -3,16 +3,17 @@ package ru.npn.spring.service.demo.controller;
 import com.querydsl.core.types.Predicate;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.npn.spring.service.demo.controller.dto.ProductTypeCreateDto;
 import ru.npn.spring.service.demo.controller.dto.ProductTypeDto;
+import ru.npn.spring.service.demo.domain.ProductTypeEntity;
 import ru.npn.spring.service.demo.service.producttype.ProductTypeMainService;
 
 import java.util.UUID;
@@ -36,8 +37,22 @@ public class ProductTypeController {
 
   @GetMapping("")
   @Operation(summary = "Поиск типов продукта")
-  Page<ProductTypeDto> findProductByParam(@QuerydslPredicate(root = ProductTypeDto.class) Predicate predicate, Pageable pageable){
+  @Parameters(value = {
+      @Parameter(name = "productTypeId", description = "Идентификатор", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6",schema = @Schema(type = "string", format = "uuid")),
+      @Parameter(name = "name", description = "Наименование", example = "name",schema = @Schema(type = "string")),
+      @Parameter(name = "page", description = "Наименование", example = "0",schema = @Schema(type = "integer")),
+      @Parameter(name = "size", description = "Наименование", example = "100",schema = @Schema(type = "integer")),
+      @Parameter(name = "sort", description = "Наименование", schema = @Schema(type = "string")),
+  })
+  Page<ProductTypeDto> findProductByParam(@Parameter(hidden = true) @QuerydslPredicate(root = ProductTypeEntity.class) Predicate predicate, @Parameter(hidden = true) Pageable pageable){
     return productTypeMainService.findProductByParam(predicate, pageable);
+  }
+
+
+  @PostMapping("")
+  @Operation(summary = "Создание типа продукта")
+  ProductTypeDto createProductType(@RequestBody(required = true) ProductTypeCreateDto createDto){
+    return null;
   }
 
 
---
+++ service/src/main/java/ru/npn/spring/service/demo/controller/dto/ProductTypeCreateDto.java
@@ -0,0 +1,22 @@
+package ru.npn.spring.service.demo.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author Новоселов Павел
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductTypeCreateDto implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @JsonProperty("name")
+  private String name;
+
+}
